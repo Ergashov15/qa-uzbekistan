bd1bbc48a0df71a1747edc20f0bbff18e54152d3
Sherzod Ergashov <sherzodergashov1511@gmail.com>
2026-09-18 16:56:11 +0500

    Add NotFoundPage component

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+import { NotFoundPage } from 'nextra-theme-docs'
+
+export default function NotFound() {
+  return (
+    <NotFoundPage content="Ishlamayotgan havola bo'yicha GitHub'da xabar qoldiring" labels="broken-link">
+      <h1>404: Sahifa topilmadi</h1>
+    </NotFoundPage>
+  )
+}
