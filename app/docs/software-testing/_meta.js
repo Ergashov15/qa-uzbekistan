8ca8e388bf8b48bcff0b6daa521e61269852862d
Sherzod Ergashov <sherzodergashov1511@gmail.com>
2026-09-19 13:27:50 +0500

    feat: translate and update Levels of Testing page and navigation to Uzbek

--- app/docs/software-testing/_meta.js
+++ app/docs/software-testing/_meta.js
@@ -5,7 +5,7 @@ export default {
   'software-development-life-cycle': "Dasturiy Ta'minotni Ishlab Chiqish Hayotiy Davri (SDLC)",
   'software-testing-life-cycle': "Dasturiy Ta'minotni Sinash Hayotiy Davri (STLC)",
   'types-of-software-testing': "Dasturiy Ta'minotni Sinash Turlari",
-  'levels-of-testing': 'Levels of Testing',
+  'levels-of-testing': "Dasturiy Ta'minotni Sinash Darajalari",
   'test-maturity-model': 'Test Maturity Model',
   'api-testing-tutorial': 'API Testing Tutorial',
   'acceptance-test-driven-development': 'Acceptance Test Driven Development(ATDD)',
