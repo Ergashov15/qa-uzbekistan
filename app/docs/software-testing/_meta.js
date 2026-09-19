ee6a782b5f799db9938b0b7e5099820805fa00dd
Sherzod Ergashov <sherzodergashov1511@gmail.com>
2026-09-19 13:04:56 +0500

    docs: translate Software Testing Principles title to Uzbek across app

--- app/docs/software-testing/_meta.js
+++ app/docs/software-testing/_meta.js
@@ -1,7 +1,7 @@
 export default {
   index: 'Software Testing Asoslari',
   'software-testing-tutorial': 'Dasturiy Ta\'minotni Sinash Qo\'llanmasi (Software Testing Tutorial)',
-  'software-testing-principles': 'Software Testing Principles',
+  'software-testing-principles': "Dasturiy Ta'minotni Sinash Tamoyillari",
   'software-development-life-cycle': 'Software Development Life Cycle',
   'software-testing-life-cycle': 'Software Testing Life Cycle',
   'types-of-software-testing': 'Types of Software Testing',
