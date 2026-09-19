1376049d3625c74ee752b1d867d0a2f55b621ad4
Sherzod Ergashov <sherzodergashov1511@gmail.com>
2026-09-19 13:35:09 +0500

    docs: remove test-maturity-model section and renumber navigation cards

--- app/docs/software-testing/_meta.js
+++ app/docs/software-testing/_meta.js
@@ -6,7 +6,6 @@ export default {
   'software-testing-life-cycle': "Dasturiy Ta'minotni Sinash Hayotiy Davri (STLC)",
   'types-of-software-testing': "Dasturiy Ta'minotni Sinash Turlari",
   'levels-of-testing': "Dasturiy Ta'minotni Sinash Darajalari",
-  'test-maturity-model': 'Test Maturity Model',
   'api-testing-tutorial': 'API Testing Tutorial',
   'acceptance-test-driven-development': 'Acceptance Test Driven Development(ATDD)',
   'methodological-types': 'Methodological Types'
