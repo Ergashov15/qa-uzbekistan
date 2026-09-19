553566426f3cc6ae2661eb271140c66b11dec605
Sherzod Ergashov <sherzodergashov1511@gmail.com>
2026-09-19 15:27:31 +0500

    feat: rewrite Hybrid Model in Software Testing in Uzbek

--- app/docs/sdlc-models/_meta.js
+++ app/docs/sdlc-models/_meta.js
@@ -2,7 +2,7 @@ export default {
   index: 'SDLC Modellari Asoslari',
   'waterfall-model': 'Sharshara Modeli (Waterfall Model in Software Testing)',
   'spiral-model': 'Spiral Modeli (Spiral Model in Software Testing)',
-  'hybrid-model': 'Gibrid Model (Hybrid Model)',
+  'hybrid-model': 'Gibrid Model (Hybrid Model in Software Testing)',
   'prototype-model': 'Prototip Modeli (Prototype Model)',
   'v-model': 'V-Model (Verification & Validation)'
 }
