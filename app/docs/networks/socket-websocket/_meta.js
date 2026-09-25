dc08e320c6785c4623291c9dc57a62eb9f6a91ab
Sherzod Ergashov <sherzodergashov1511@gmail.com>
2026-09-25 13:37:32 +0500

    docs: add test documentation subpages and update networks documentation in Uzbek

---
+++ app/docs/networks/socket-websocket/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  index: 'Soket va Veb-soket (Kirish)',
+  'socket-websocket': 'Soket va Veb-soket (Socket / WebSocket)',
+  'testing-websockets-on-clients': 'Mijozlarda Veb-soketlarni Sinash (Testing WebSockets on Clients)'
+}
---
+++ app/docs/test-documentation/_meta.js
@@ -0,0 +1,19 @@
+export default {
+  index: 'Test Hujjatlari Asoslari',
+  'quality-policy': 'Sifat Siyosati (Quality Policy)',
+  'test-policy': 'Test Siyosati (Test Policy)',
+  'test-strategy': 'Test Strategiyasi (Test Strategy)',
+  'test-plan': 'Test Rejasi (Test Plan)',
+  'test-scenario': 'Test Ssenariysi (Test Scenario)',
+  'test-case': 'Test Keys (Test Case)',
+  checklist: 'Chek-list (Checklist)',
+  'bug-report': 'Xatolik Hisoboti (Bug Report)',
+  requirements: 'Talablar (Requirements)',
+  'user-stories': 'Foydalanuvchi Tarixlari (User Stories)',
+  'acceptance-criteria': 'Qabul Qilish Mezonlari (Acceptance Criteria)',
+  'test-basis': 'Test Bazasi (Test Basis)',
+  'types-of-test-reports': 'Test Hisobotlari Turlari (Types of Test Reports)',
+  'requirement-traceability-matrix': 'Talablar Kuzatuvchanlik Matritsasi (RTM)',
+  'software-test-metrics': 'Dasturiy Sinov Metrikalari (Software Test Metrics)',
+  'test-oracle': 'Test Orakuli (Test Oracle)'
+}
